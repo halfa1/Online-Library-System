ab2f0e20c925854ddb450536e1308e613d39c417
halfamaton ngesu <ngesuhalfa5@gmail.com>
2025-01-19 11:44:15 +0300

    added deployment settings and other adjustments

--- online_library/settings.py
+++ online_library/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
 from pathlib import Path
+from django.contrib import staticfiles
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@h4um5fxo=)hc*o#1z)pwk=pvj+b_1@%q-7_0g_66&*p2@431%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+#DEBUG = True
+DEBUG = False
+ALLOWED_HOSTS = ['127.0.0.1' ,'online_library.onrender.com']
 
 
 # Application definition
@@ -119,6 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 #STATIC_URL = '/static/'
 # Static and Media Settings
---
+++ staticfiles/Procfile
@@ -0,0 +1,1 @@
+web: gunicorn online_library.wsgi:application
